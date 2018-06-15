a1fa4fcd4d82a291582f008e22f0a083bd2cb9be
Mateusz Czapliński <czapkofan@gmail.com>
2018-06-15 22:20:40 +0200

    MessageQueue.spy

--- App.js
+++ App.js
@@ -1,2 +1,15 @@
+import MessageQueue from 'react-native/Libraries/BatchedBridge/MessageQueue.js';
+MessageQueue.spy(info => {
+  var name = `${info.module ? info.module + '.' : ''}${info.method}`;
+  if (name !== 'RCTDeviceEventEmitter.emit' &&
+    name !== 'Networking.sendRequest') {
+    // console.log(name);
+    console.log(`${info.type === 0 ? 'N->JS' : 'JS->N'} : ` +
+      `${info.module ? info.module + '.' : ''}${info.method}` +
+      `(${JSON.stringify(info.args)})`,
+    );
+  }
+});
+
 const Elm = require('./elm');
 export default Elm.Main.start();
